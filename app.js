6935c364c19006d86c26720a5f909bfcb43f1365
moonshadow <last_enemy@naver.com>
2020-11-11 19:46:06 +0900

    201111-1

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-// import path from "path"
 const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -8,15 +7,22 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  devtool: "cheap-module-source-map",
+  entry: ["babel-polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
         test: /\.(scss)$/,
         use: [
           {
-            loader: MiniCssExtractPlugin,
+            loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
@@ -26,12 +32,19 @@ const config = {
             options: {
               postcssOptions: {
                 plugins: [
-                  ["autoprefixer", { overrideBrowserslist: "cover 99.5%" }],
+                  [
+                    "autoprefixer",
+                    {
+                      overrideBrowserslist: "cover 99.5%",
+                    },
+                  ],
                 ],
               },
             },
           },
-          { loader: "sass-loader" },
+          {
+            loader: "sass-loader",
+          },
         ],
       },
     ],
